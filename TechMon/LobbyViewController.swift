cd7456a94aec1256d0451533fcf98c2d507db1a4
Korissa. RisakoSuzuki <okasi_aru570@icloud.com>
2023-05-10 19:25:07 +0900

    Initial Commit

---
+++ TechMon/LobbyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LobbyViewController.swift
+//  TechMon
+//
+//  Created by 鈴木理紗子 on 2023/05/10.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        TechMonManager.playBGM(fileName: "lobby")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        TechMonManager.stopBGM()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 鈴木理紗子 on 2023/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
